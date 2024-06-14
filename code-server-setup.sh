70b7b3b611d5f48f576ed30d3afdcea2f6cc8b04
sumgang45 <102499046+sumgang45@users.noreply.github.com>
2024-06-14 16:38:04 +0530

    starting codeserver after installations are complete

--- code-server-setup.sh
+++ code-server-setup.sh
@@ -9,23 +9,34 @@ sudo systemctl enable --now code-server@root
 command -v code-server >/dev/null 2>&1 || { echo >&2 "code-server not installed."; exit 1; }
 
 # Run code-server
-echo "Running code-server to populate defaults"
-sudo systemctl start code-server@root
-sleep 5
+# echo "Running code-server to populate defaults"
+# sudo systemctl start code-server@root
+# sleep 5
 
 # Set custom theme
 echo "Setting dark theme for code server"
 sudo mkdir -p /root/.local/share/code-server/User
-sudo tee /root/.local/share/code-server/User/settings.json <<EOF
+sudo tee /root/.local/share/code-server/User/settings.json <<EOF1
 {
     "workbench.colorTheme": "Default Dark Modern"
 }
-EOF
+EOF1
+
+# Set path to open to
+echo "Setting path to open code server to"
+sudo mkdir -p /root/.local/share/code-server
+sudo tee /root/.local/share/code-server/coder.json  <<EOF2
+{
+  "query": {
+    "folder": "/root"
+  }
+}
+EOF2
 
 # Configure code-server
 echo "Configuring code-server"
 sudo mkdir -p /root/.config/code-server
-sudo tee /root/.config/code-server/config.yaml <<EOF
+sudo tee /root/.config/code-server/config.yaml <<EOF3
 bind-addr: 127.0.0.1:2059
 auth: none
 cert: false
@@ -33,9 +44,9 @@ disable-update-check: true
 disable-getting-started-override: true
 disable-workspace-trust: true
 force: true
-EOF
+EOF3
 
 # Run code-server
 echo "Running code-server with the new configuration"
-sudo systemctl restart code-server@root
+sudo systemctl start code-server@root
 echo "Setup complete"
